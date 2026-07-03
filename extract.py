0afbc1b59c0b5b46330bceb8be587f266c37ac47
Felipe-Magalhaes-A-Carneiro <felipe.araujo.arq@outlook.com>
2026-07-02 21:49:48 -0300

    feat(extract): cria a extracao de dados

---
+++ extract.py
@@ -0,0 +1,13 @@
+import requests
+import pandas as pd
+from datetime import datetime
+
+def extrair_dados():
+    url = "https://awesomeapi.com.br"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        dados = response.json()
+        return dados
+    else:
+        raise Exception(f"Erro ao acessar API: {response.status_code}")
